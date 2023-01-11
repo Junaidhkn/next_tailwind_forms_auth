0bc219189b1b0b3d1cb401047cd0947cffb949b9
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-11 22:19:50 +0500

    style fixes

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -12,37 +12,61 @@ const register = () => {
 				</div>
 				<div className='mt-10'>
 					<form>
-						<div className='flex flex-col mb-5'>
+						<div className='flex flex-col mb-6'>
 							<label
 								htmlFor='name'
-								className='mb-1 text-xs tracking-wide text-gray-600'>
+								className='mb-1 text-xs sm:text-sm tracking-wide text-gray-600'>
 								Name:
 							</label>
 							<div className='relative'>
-								<div className='inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400'></div>
+								<div className='inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400'>
+									<svg
+										className='h-6 w-6'
+										fill='none'
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										viewBox='0 0 24 24'
+										stroke='currentColor'>
+										<path d='M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z' />
+									</svg>
+								</div>
+
 								<input
 									id='name'
-									type='text'
+									type='name'
 									name='name'
-									className=' text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400'
+									className='text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400'
 									placeholder='Enter your name'
 								/>
 							</div>
 						</div>
-						<div className='flex flex-col mb-5'>
+						<div className='flex flex-col mb-6'>
 							<label
 								htmlFor='email'
-								className='mb-1 text-xs tracking-wide text-gray-600'>
+								className='mb-1 text-xs sm:text-sm tracking-wide text-gray-600'>
 								E-Mail Address:
 							</label>
 							<div className='relative'>
-								<div className='inline-flex  items-center  justify-center  absolute  left-0  top-0  h-full  w-10  text-gray-400'></div>
+								<div className='inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400'>
+									<svg
+										className='h-6 w-6'
+										fill='none'
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										viewBox='0 0 24 24'
+										stroke='currentColor'>
+										<path d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207' />
+									</svg>
+								</div>
+
 								<input
 									id='email'
 									type='email'
 									name='email'
-									className='text-sm  placeholder-gray-500  pl-10  pr-4  rounded-2xl  border border-gray-400  w-full  py-2  focus:outline-none focus:border-blue-400'
-									placeholder='Enter your email'
+									className='text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400'
+									placeholder='E-Mail Address'
 								/>
 							</div>
 						</div>
@@ -53,15 +77,27 @@ const register = () => {
 								Password:
 							</label>
 							<div className='relative'>
-								<div className='inline-flex  items-center  justify-center  absolute  left-0  top-0  h-full  w-10  text-gray-400'>
-									<span></span>
+								<div className='inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400'>
+									<span>
+										<svg
+											className='h-6 w-6'
+											fill='none'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											viewBox='0 0 24 24'
+											stroke='currentColor'>
+											<path d='M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z' />
+										</svg>
+									</span>
 								</div>
+
 								<input
 									id='password'
 									type='password'
 									name='password'
-									className='text-sm  placeholder-gray-500  pl-10  pr-4 last:rounded-2xl  border border-gray-400  w-full  py-2  focus:outline-none focus:border-blue-400'
-									placeholder='Enter your password'
+									className='text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400'
+									placeholder='Password'
 								/>
 							</div>
 						</div>
