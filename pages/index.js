632df185bc423e6d9cd95ceb0dd2b4885bda7df1
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-13 01:01:56 +0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ const HomePage = () => {
 	return (
 		<div className='my-12 mx-auto text-center'>
 			<h1 className='m-auto text-[50px]'>Welcome to Home Page!</h1>
-			<div className='flex flex-col justify-center items-center mt-4 self-center text-xl sm:text-sm text-gray-800'>
-				<h2 className='font-4xl font-bold'>Profile Page (Authorized!)</h2>
-				<h3>{session.user.name}</h3>
-				<h3>{session.user.email}</h3>
-			</div>
+			{session && (
+				<div className='flex flex-col justify-center items-center mt-4 self-center text-xl sm:text-sm text-gray-800'>
+					<h2 className='font-4xl font-bold'>Profile Page (Authorized!)</h2>
+					<h3>{session.user.name}</h3>
+					<h3>{session.user.email}</h3>
+				</div>
+			)}
 		</div>
 	);
 };
