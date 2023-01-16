a8de889c73f342e684eeba6987cd7aa8e3cc81f0
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-16 19:45:52 +0500

    commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,33 @@ export default NextAuth( {
       GoogleProvider( {
          clientId: process.env.GOOGLE_ID,
          clientSecret: process.env.GOOGLE_SECRET
+      } ),
+      CredentialsProvider( {
+         name: "Credentials",
+         async authorize ( credentials, req ) {
+            connectMongo().catch( error => { error: "Connection Failed...!" } )
+
+            // check user existance
+            const result = await Users.findOne( { email: credentials.email } )
+            if ( !result ) {
+               throw new Error( "No user Found with Email Please Sign Up...!" )
+            }
+
+            // compare()
+            const checkPassword = await compare( credentials.password, result.password );
+
+            // incorrect password
+            if ( !checkPassword || result.email !== credentials.email ) {
+               throw new Error( "Username or Password doesn't match" );
+            }
+
+            return result;
+
+         }
       } )
-   ]
+   ],
+   secret: process.env.JWT_SECRET,
+   session: {
+      strategy: 'jwt',
+   }
 } )
